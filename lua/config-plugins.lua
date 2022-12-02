16e079ff6bb1e970def28f70a35315980c2f2973
lemon0910 <lemon9010@gmail.com>
2022-12-02 16:25:16 +0800

    修改telescope配置

--- lua/config-plugins.lua
+++ lua/config-plugins.lua
@@ -96,7 +96,31 @@ require("bufferline").setup {
 }
 
 -- telescope
--- map("n", "<leader>ff", "<cmd>Telescope find_files<cr>", opt)
+local builtin = require('telescope.builtin')
+local actions = require("telescope.actions")
+require("telescope").setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-u>"] = false,
+        ["<esc>"] = actions.close
+      },
+    },
+  },
+  pickers = {
+      find_files = {
+          -- theme = "dropdown",
+      }
+  }
+}
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fr', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
+vim.keymap.set('n', '<leader>fm', builtin.marks, {})
+vim.keymap.set('n', '<leader>fq', builtin.quickfix, {})
+vim.keymap.set('n', '<leader>fs', builtin.current_buffer_fuzzy_find, {})
 
 -- hop.vim
 require("hop").setup()
