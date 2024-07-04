d3cac7476bb52ea382587b7ce3ec8a28656c03b5
zhixin.lm <zhixin.lm@antgroup.com>
2024-07-04 10:07:08 +0800

    增加 clangd 和 pyright 配置

--- lua/config.lua
+++ lua/config.lua
@@ -554,6 +554,63 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
 require("lspconfig").gopls.setup{}
 
+local util = require 'lspconfig.util'
+
+-- https://clangd.llvm.org/extensions.html#switch-between-sourceheader
+local function switch_source_header(bufnr)
+  bufnr = util.validate_bufnr(bufnr)
+  local clangd_client = util.get_active_client_by_name(bufnr, 'clangd')
+  local params = { uri = vim.uri_from_bufnr(bufnr) }
+  if clangd_client then
+    clangd_client.request('textDocument/switchSourceHeader', params, function(err, result)
+      if err then
+        error(tostring(err))
+      end
+      if not result then
+        print 'Corresponding file cannot be determined'
+        return
+      end
+      vim.api.nvim_command('edit ' .. vim.uri_to_fname(result))
+    end, bufnr)
+  else
+    print 'method textDocument/switchSourceHeader is not supported by any servers active on the current buffer'
+  end
+end
+
+local root_files = {
+  '.clangd',
+  '.clang-tidy',
+  '.clang-format',
+  'compile_commands.json',
+  'compile_flags.txt',
+  'configure.ac', -- AutoTools
+}
+
+local default_capabilities = {
+  textDocument = {
+    completion = {
+      editsNearCursor = true,
+    },
+  },
+  offsetEncoding = { 'utf-8', 'utf-16' },
+}
+
+require("lspconfig").clangd.setup{
+    cmd = { '/usr/bin/clangd' },
+    filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda', 'proto' },
+    root_dir = function(fname)
+      return util.root_pattern(unpack(root_files))(fname) or util.find_git_ancestor(fname)
+    end,
+    single_file_support = true,
+    capabilities = default_capabilities,
+}
+
+require("lspconfig").pyright.setup({
+  cmd = {'/usr/local/bin/pyright-langserver', '--stdio'},
+  filetypes = { 'python' },
+  single_file_support = true,
+})
+
 require("notify").setup({
   timeout = 2000,
   stages = 'static'
