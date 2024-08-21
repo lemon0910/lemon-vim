562a9291739ca2d8466c36949739cf6c21140b45
zhixin.lm <zhixin.lm@antgroup.com>
2024-08-21 14:20:25 +0800

    treesitter 支持 go 和 python

--- lua/config.lua
+++ lua/config.lua
@@ -234,7 +234,7 @@ require('nvim-autopairs').setup{}
 require'nvim-treesitter.configs'.setup {
   -- 安装 language parser
   -- :TSInstallInfo 命令查看支持的语言
-  ensure_installed = {"c", "cpp", "vim", "lua", },
+  ensure_installed = {"c", "cpp", "vim", "lua", "go", "python" },
   ignore_install = { "vim" },
   -- 启用代码高亮功能
   highlight = {
