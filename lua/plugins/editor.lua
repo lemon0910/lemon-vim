39262f440af86099b10532795a4a70869065e44a
zhixin.lm <zhixin.lm@oceanbase.com>
2025-01-13 11:00:18 +0800

    增加 yazi 的配置

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -250,4 +250,30 @@ return {
       { "<leader>ps", '<cmd>lua require("persistence").load()<cr>' },
     }
   },
+
+  {
+    dir = "~/.local/share/nvim/lazy/yazi.nvim",
+    event = "VeryLazy",
+    keys = {
+      -- 👇 in this section, choose your own keymappings!
+      {
+        "<leader>-",
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+      {
+        -- Open in the current working directory
+        "<leader>cw",
+        "<cmd>Yazi cwd<cr>",
+        desc = "Open the file manager in nvim's working directory" ,
+      },
+      {
+        -- NOTE: this requires a version of yazi that includes
+        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+        '<c-up>',
+        "<cmd>Yazi toggle<cr>",
+        desc = "Resume the last yazi session",
+      },
+    },
+  },
 }
