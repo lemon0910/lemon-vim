0037ca24f7054e6bc281f9d901f2829a1e4249ad
zhixin.lm <zhixin.lm@antgroup.com>
2025-01-19 15:01:18 +0800

    增加复制时高亮的 autocmd

--- lua/options.lua
+++ lua/options.lua
@@ -183,3 +183,19 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.opt_local.tabstop = 2
   end,
 })
+
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    (vim.hl or vim.highlight).on_yank()
+  end,
+})
+
+-- resize splits if window got resized
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    local current_tab = vim.fn.tabpagenr()
+    vim.cmd("tabdo wincmd =")
+    vim.cmd("tabnext " .. current_tab)
+  end,
+})
