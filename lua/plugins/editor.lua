9db47ce584129be0495adba314228e05b1b61ab9
zhixin.lm <zhixin.lm@oceanbase.com>
2025-02-21 17:06:05 +0800

    调整 gitsigns 配置

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -56,15 +56,14 @@ return {
       linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
       word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
       watch_gitdir = {
-        interval = 1000,
         follow_files = true
       },
-      attach_to_untracked = true,
+      attach_to_untracked = false,
       current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-        delay = 500,
+        delay = 1000,
         ignore_whitespace = false,
       },
       current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
