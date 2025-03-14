950d759d150bf6fbe0db8dc23191714a0b627faf
zhixin.lm <zhixin.lm@oceanbase.com>
2025-03-14 16:35:55 +0800

    更新 dropbar 的快捷键

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -220,6 +220,10 @@ return {
   {
     dir = '~/.local/share/nvim/lazy/dropbar.nvim',
     -- optional, but required for fuzzy finder support
-    opts = {},
+    config = function ()
+      local dropbar_api = require('dropbar.api')
+      vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+      vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+    end
   },
 }
